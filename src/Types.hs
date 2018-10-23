561adcc90e207725737d34d3633862b1137c362c
VladislavTitov <titovvlad1997@yandex.ru>
2018-10-23 10:26:18 +0300

    initial types

---
+++ src/Types.hs
@@ -0,0 +1,22 @@
+data Direction = North
+               | South
+               | East
+               | West
+               deriving (Show, Eq)
+
+data Command = Quit | Go Direction deriving (Show, Eq)
+
+type Position = (Int, Int)
+type Snake = [Position]
+
+data World = World { snake :: Snake
+                   , food :: Position
+                   , direction :: Direction
+                   , rand :: R.StdGen
+                   , limits :: (Int, Int)
+                   } deriving (Show)
+
+data GameState = Playing World
+               | GameOver
+               deriving (Show)
+
